8e7c9c66faeba3fd6d288d53b8e2de9ccd81110c
LordSword <sword_lord@foxmail.com>
2021-09-24 15:05:43 +0800

    initial commit

---
+++ DontSleep/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  test1
+//
+//  Created by Sword on 2021/9/23.
+//
+
+import Cocoa
+
+let app = NSApplication.shared
+let appDelegate = AppDelegate()
+app.delegate = appDelegate
+
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
